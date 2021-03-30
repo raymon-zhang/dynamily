d45378d2b735661eec704d67cc902c8ca37e8540
Raymon Zhang <raymonzhang20@gmail.com>
2021-03-29 19:39:28 -0700

    Changed featured image to absolute url

--- components/Metatags.js
+++ components/Metatags.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function Metatags({
     title = "Dynamily",
     description = "Dynamily helps you manage and connect your family's everyday life.",
-    image = "/images/featured.png",
+    image = process.env.VERCEL_URL + "/images/featured.png",
 }) {
     return (
         <Head>
