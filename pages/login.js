b55bee41508550982b634692948251374545578c
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-06-22 14:50:51 -0700

    TEST: Add console log to login

--- pages/login.js
+++ pages/login.js
@@ -61,6 +61,7 @@ function SignInButton(props) {
             .signInWithPopup(googleAuthProvider)
             .catch((e) => {
                 toast.error("Oops! There was an error.");
+                console.log(e);
             })
             .then(() => {
                 toast.success("Signed in!");
