74a6ce33b7235ce2fed09b283fb3030ae91a65a6
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-07-07 12:49:14 -0700

    Make sticky header icon change depending on page

--- components/Family/PanelStickyHeader.js
+++ components/Family/PanelStickyHeader.js
@@ -1,19 +1,44 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import styles from "./PanelStickyHeader.module.scss";
 import utilStyles from "@styles/utils.module.scss";
 
-import HomeIcon from "@icons/home.svg";
+import MessageIcon from "@icons/message.svg";
+import GalleryIcon from "@icons/image.svg";
+import TodoIcon from "@icons/check-square.svg";
+import ShoppingIcon from "@icons/shopping-cart.svg";
 
-export default function PanelStickyHeader({ children }) {
+export default function PanelStickyHeader({ page, children }) {
+    const pageOptions = {
+        messages: {
+            url: "/family",
+            icon: <MessageIcon />,
+        },
+        gallery: {
+            url: "/family/gallery",
+            icon: <GalleryIcon />,
+        },
+        todos: {
+            url: "/family/todos",
+            icon: <TodoIcon />,
+        },
+        shopping: {
+            url: "/family/shopping",
+            icon: <ShoppingIcon />,
+        },
+    };
+    const currentPage = pageOptions[page];
+    delete pageOptions[page];
+
     return (
         <div className={styles.stickyHeader}>
-            <Link href="/family">
-                <a className={styles.headerContent}>
-                    <h3 className={utilStyles.headingLg}>{children}</h3>
-                    <HomeIcon />
-                </a>
-            </Link>
+            <div className={styles.headerContent}>
+                <h3 className={utilStyles.headingLg}>{children}</h3>
+                <Link href={currentPage.url}>
+                    <a>{currentPage.icon}</a>
+                </Link>
+            </div>
         </div>
     );
 }
