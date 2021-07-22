34291f65cc5ab576ace53dc909dfb75b99e82300
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-07-21 20:31:28 -0700

    Super simple landing page

--- layouts/LandingPage.js
+++ layouts/LandingPage.js
@@ -3,7 +3,17 @@ import styles from "./LandingPage.module.scss";
 export default function LandingPage() {
     return (
         <div className={styles.landingPage}>
-            <h1>Landing Page</h1>
+            <section className={styles.hero}>
+                <div className={styles.heroInner}>
+                    <h1 className={styles.heroHeading}>
+                        Keep your family organized
+                    </h1>
+                    <p className={styles.heroSubHeading}>
+                        Dynamily helps you manage and connect your family's
+                        everyday life.
+                    </p>
+                </div>
+            </section>
         </div>
     );
 }
