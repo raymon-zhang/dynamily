8543cd69e295544635453e5152236484975606b4
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-07-22 19:19:16 -0700

    Add more content to landing page

--- layouts/LandingPage.js
+++ layouts/LandingPage.js
@@ -1,3 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import motherImage from "../public/images/family_mother.jpg";
+import fatherImage from "../public/images/family_father.jpg";
+import olderSonImage from "../public/images/family_olderSon.jpg";
+import youngerSonImage from "../public/images/family_youngerSon.jpg";
+
 import styles from "./LandingPage.module.scss";
 
 export default function LandingPage() {
@@ -9,9 +17,74 @@ export default function LandingPage() {
                         Keep your family organized
                     </h1>
                     <p className={styles.heroSubHeading}>
-                        Dynamily helps you manage and connect your family's
-                        everyday life.
+                        It can be difficult to keep your family organized.
+                        Dynamily empowers you manage and connect your family's
+                        everyday life, for free.
                     </p>
+                    <Link href="/login">
+                        <button
+                            className={`${styles.heroButton} ${styles.heroCta}`}
+                        >
+                            Sign up today
+                        </button>
+                    </Link>
+                    <Link href="/login">
+                        <button
+                            className={`${styles.heroButton} ${styles.heroSecondary}`}
+                        >
+                            Learn more
+                        </button>
+                    </Link>
+                </div>
+                <div className={`${styles.familyImage} ${styles.familyMother}`}>
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src={motherImage}
+                            width={144}
+                            height={144}
+                            alt={"Mother"}
+                            placeholder="blur"
+                        />
+                    </div>
+                </div>
+                <div className={`${styles.familyImage} ${styles.familyFather}`}>
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src={fatherImage}
+                            width={144}
+                            height={144}
+                            alt={"Father"}
+                            placeholder="blur"
+                        />
+                    </div>
+                    <div className={styles.dots} />
+                </div>
+                <div
+                    className={`${styles.familyImage} ${styles.familyOlderSon}`}
+                >
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src={olderSonImage}
+                            width={144}
+                            height={144}
+                            alt={"Older Son"}
+                            placeholder="blur"
+                        />
+                    </div>
+                    <div className={styles.dots} />
+                </div>
+                <div
+                    className={`${styles.familyImage} ${styles.familyYoungerSon}`}
+                >
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src={youngerSonImage}
+                            width={144}
+                            height={144}
+                            alt={"Younger Son"}
+                            placeholder="blur"
+                        />
+                    </div>
                 </div>
             </section>
         </div>
