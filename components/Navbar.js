2e3364f0abf84506582da62cc9e95eb683763a79
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-07-28 19:15:14 -0700

    Add a different navbar for when not logged in

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,6 +7,8 @@ import { useTheme } from "next-themes";
 import { UserContext } from "@lib/context";
 import { signOut } from "@lib/firebase";
 
+import NavbarNotLoggedIn from "./NavbarNotLoggedIn";
+
 import styles from "./Navbar.module.scss";
 import utilStyles from "@styles/utils.module.scss";
 
@@ -24,73 +26,60 @@ export default function Navbar() {
 
     const router = useRouter();
 
+    if (!username) {
+        return <NavbarNotLoggedIn />;
+    }
     return (
         <nav className={styles.navbar}>
             <ul>
                 <li>
                     <Link href="/">
-                        <a className={styles.logo}>
+                        <a className="logo">
                             <Logo />
                         </a>
                     </Link>
                 </li>
 
-                {/* user is signed-in and has username */}
-                {username && (
-                    <>
-                        <li className={utilStyles.pushLeft}>
-                            <div
-                                tabIndex={0}
-                                role="button"
-                                className={styles.profileButton}
-                            >
-                                <img src={user?.photoURL} />
-                                <div className={styles.dropdown}>
-                                    <Link href={`/u/${username}`}>
-                                        <a className={styles.dropdownItem}>
-                                            <UserIcon /> Profile
-                                        </a>
-                                    </Link>
-                                    <button className={styles.dropdownItem}>
-                                        <BrushIcon /> Dark mode{" "}
-                                        <label className={styles.switch}>
-                                            <input
-                                                type="checkbox"
-                                                checked={darkModeActive}
-                                                onChange={() => {
-                                                    darkModeActive
-                                                        ? setTheme("light")
-                                                        : setTheme("dark");
-                                                }}
-                                            />
-                                            <span
-                                                className={styles.slider}
-                                            ></span>
-                                        </label>
-                                    </button>
-                                    <button
-                                        onClick={() => {
-                                            signOut(router);
+                <li className={utilStyles.pushLeft}>
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        className={styles.profileButton}
+                    >
+                        <img src={user?.photoURL} />
+                        <div className={styles.dropdown}>
+                            <Link href={`/u/${username}`}>
+                                <a className={styles.dropdownItem}>
+                                    <UserIcon /> Profile
+                                </a>
+                            </Link>
+                            <button className={styles.dropdownItem}>
+                                <BrushIcon /> Dark mode{" "}
+                                <label className={styles.switch}>
+                                    <input
+                                        type="checkbox"
+                                        checked={darkModeActive}
+                                        onChange={() => {
+                                            darkModeActive
+                                                ? setTheme("light")
+                                                : setTheme("dark");
                                         }}
-                                        className={styles.dropdownItem}
-                                    >
-                                        <LogoutIcon />
-                                        Sign Out
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-                    </>
-                )}
-
-                {/* user is not signed OR has not created username */}
-                {!username && (
-                    <li>
-                        <Link href="/login">
-                            <button className="btn-blue-light">Log in</button>
-                        </Link>
-                    </li>
-                )}
+                                    />
+                                    <span className={styles.slider}></span>
+                                </label>
+                            </button>
+                            <button
+                                onClick={() => {
+                                    signOut(router);
+                                }}
+                                className={styles.dropdownItem}
+                            >
+                                <LogoutIcon />
+                                Sign Out
+                            </button>
+                        </div>
+                    </div>
+                </li>
             </ul>
         </nav>
     );
---
+++ components/NavbarNotLoggedIn.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+import styles from "./NavbarNotLoggedIn.module.scss";
+import utilStyles from "@styles/utils.module.scss";
+
+import Logo from "@icons/Logo.svg";
+
+// Top navbar
+export default function NavbarNotLoggedIn() {
+    return (
+        <nav className={styles.navbarNotLoggedIn}>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <a className="logo">
+                            <Logo />
+                        </a>
+                    </Link>
+                </li>
+
+                {/* <li className={utilStyles.pushLeft}>
+                        </li> */}
+                <li>
+                    <Link href="/login">
+                        <button className={styles.loginButton}>Log in</button>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
